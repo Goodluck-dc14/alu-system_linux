df7912ede38a53478f1ad36b8e5d3f621bdaa815
Goodluck-dc14 <ogbonnaiheooma48@gmail.com>
2026-07-23 15:35:47 +0200

    Fix Betty style tab indentation, stdout buffering, and SO_REUSEADDR in sockets project

--- sockets/1-server.c
+++ sockets/1-server.c
@@ -13,36 +13,36 @@
  */
 int create_and_bind_socket(void)
 {
-        int server_fd;
-        struct sockaddr_in server_addr;
+	int server_fd;
+	struct sockaddr_in server_addr;
 
-        server_fd = socket(AF_INET, SOCK_STREAM, 0);
-        if (server_fd < 0)
-        {
-                perror("socket");
-                exit(EXIT_FAILURE);
-        }
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd < 0)
+	{
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
 
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(12345);
-        server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(12345);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-        if (bind(server_fd, (struct sockaddr *)&server_addr,
-                sizeof(server_addr)) < 0)
-        {
-                perror("bind");
-                close(server_fd);
-                exit(EXIT_FAILURE);
-        }
+	if (bind(server_fd, (struct sockaddr *)&server_addr,
+		sizeof(server_addr)) < 0)
+	{
+		perror("bind");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
 
-        if (listen(server_fd, 10) < 0)
-        {
-                perror("listen");
-                close(server_fd);
-                exit(EXIT_FAILURE);
-        }
+	if (listen(server_fd, 10) < 0)
+	{
+		perror("listen");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
 
-        return (server_fd);
+	return (server_fd);
 }
 
 /**
@@ -52,30 +52,30 @@ int create_and_bind_socket(void)
  */
 int main(void)
 {
-        int server_fd, client_fd;
-        socklen_t client_len;
-        struct sockaddr_in client_addr;
-        char client_ip[INET_ADDRSTRLEN];
+	int server_fd, client_fd;
+	socklen_t client_len;
+	struct sockaddr_in client_addr;
+	char client_ip[INET_ADDRSTRLEN];
 
-        server_fd = create_and_bind_socket();
+	server_fd = create_and_bind_socket();
 
-        printf("Server listening on port 12345\n");
-        fflush(stdout);
+	printf("Server listening on port 12345\n");
+	fflush(stdout);
 
-        client_len = sizeof(client_addr);
-        client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
-                &client_len);
-        if (client_fd < 0)
-        {
-                perror("accept");
-                close(server_fd);
-                return (EXIT_FAILURE);
-        }
+	client_len = sizeof(client_addr);
+	client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+		&client_len);
+	if (client_fd < 0)
+	{
+		perror("accept");
+		close(server_fd);
+		return (EXIT_FAILURE);
+	}
 
-        inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
-        printf("Client connected: %s\n", client_ip);
+	inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
+	printf("Client connected: %s\n", client_ip);
 
-        close(client_fd);
-        close(server_fd);
-        return (0);
+	close(client_fd);
+	close(server_fd);
+	return (0);
 }
